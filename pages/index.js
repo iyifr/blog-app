6a8bb9ef26b688ea5f69a0df1c1dc0c92f9d6d7b
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2023-01-10 17:27:07 +0100

    Added font awesome

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
         <meta name="description" content="A blog for weird people to feel less.. weird" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
       </Head>
 
       <>
